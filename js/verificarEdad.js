bd69cd1937d00aca445d0c86bd50d8fa76a5300b
PipeVergara <pipevergara28@hotmail.com>
2023-10-23 23:10:51 -0500

    mejoramos logica de verificar edad.js, tenia una pequeña discrepancia

--- js/verificarEdad.js
+++ js/verificarEdad.js
@@ -5,13 +5,14 @@ const outEdad = document.querySelector('#verificarEdadOutput');
 const btnCalcularEdad = document.querySelector('#verificarEdadButton');
 
 
-btnCalcularEdad.addEventListener('click',verificarEdad);
+btnCalcularEdad.addEventListener('click', verificarEdad);
 
 
 function calculaEdad(e) {
     e.preventDefault();
-   console.log(inpNacimiento.value);
-    
+    let fechaNacimiento = new Date(inpNacimiento.value);
+    let diferencia = (new Date() - fechaNacimiento) / (1000 * 60 * 60 * 24 * 365.25);
+    console.log(Math.floor(diferencia));
 }
 
 function verificarEdad(e) {
@@ -20,15 +21,16 @@ function verificarEdad(e) {
     let isMayorEdad = false;
     let diferencia;
     while (isMayorEdad === false) {
-        diferencia = (new Date() - fechaNacimiento) / (1000 * 60 * 60 * 24 * 365);
+        diferencia = (new Date() - fechaNacimiento) / (1000 * 60 * 60 * 24 * 365.25);
         if (diferencia > 18) {
             isMayorEdad = true;
+            outEdad.classList.remove('inactive');
             outEdad.innerText = 'Tu fecha de nacimiento es: ' + (fechaNacimiento.getDate() + 1) + '/' + (fechaNacimiento.getMonth() + 1) + '/' + (fechaNacimiento.getFullYear()) + ' por tanto tienes ' + Math.floor(diferencia) + ' años';
             return
         } else {
             console.log('no eres mayor de edad');
-           /*  fechaNacimiento = new Date(prompt('Por Favor digitala nuevamente (aaaa-mm-dd):')); */
-           return
+            /*  fechaNacimiento = new Date(prompt('Por Favor digitala nuevamente (aaaa-mm-dd):')); */
+            return
         }
     }
 }
