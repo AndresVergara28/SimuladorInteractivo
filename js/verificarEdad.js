52fcbe123caf56d5fe605515831f0516f97371a7
PipeVergara <pipevergara28@hotmail.com>
2023-10-27 01:08:41 -0500

    seguimos mejorando la logica

--- js/verificarEdad.js
+++ js/verificarEdad.js
@@ -3,34 +3,40 @@ const inpApellido = document.querySelector('#apellido');
 const inpNacimiento = document.querySelector('#nacimiento');
 const outEdad = document.querySelector('#verificarEdadOutput');
 const btnCalcularEdad = document.querySelector('#verificarEdadButton');
+const form = document.querySelector('#form')
 
+usuario = [];
+form.addEventListener('submit', verificar);
+form.addEventListener('reset', limpiar);
 
-btnCalcularEdad.addEventListener('click', verificarEdad);
-
-
-function calculaEdad(e) {
-    e.preventDefault();
-    let fechaNacimiento = new Date(inpNacimiento.value);
-    let diferencia = (new Date() - fechaNacimiento) / (1000 * 60 * 60 * 24 * 365.25);
-    console.log(Math.floor(diferencia));
-}
-
-function verificarEdad(e) {
-    e.preventDefault();
+function verificar(event) {
+    event.preventDefault();
+    let formulario = event.target.children;
+    let infoUsuario = {
+        nombre: formulario[0].children[1].value,
+        apellido: formulario[1].children[1].value,
+        fechaNacimiento: formulario[2].children[1].value,
+    } 
+    usuario.push(infoUsuario)
     let fechaNacimiento = new Date(inpNacimiento.value);
     let isMayorEdad = false;
     let diferencia;
     while (isMayorEdad === false) {
         diferencia = (new Date() - fechaNacimiento) / (1000 * 60 * 60 * 24 * 365.25);
         if (diferencia > 18) {
+            let edad = Math.floor(diferencia);
             isMayorEdad = true;
             outEdad.classList.remove('inactive');
-            outEdad.innerText = 'Tu fecha de nacimiento es: ' + (fechaNacimiento.getDate() + 1) + '/' + (fechaNacimiento.getMonth() + 1) + '/' + (fechaNacimiento.getFullYear()) + ' por tanto tienes ' + Math.floor(diferencia) + ' años';
+            outEdad.innerText =`${infoUsuario.nombre} ${infoUsuario.apellido} tienes ${edad} años por tanto eres Mayor de Edad. Puedes navegar sin problema`;
             return
         } else {
-            console.log('no eres mayor de edad');
-            /*  fechaNacimiento = new Date(prompt('Por Favor digitala nuevamente (aaaa-mm-dd):')); */
+            outEdad.classList.remove('inactive');
+            outEdad.innerText =`${infoUsuario.nombre} tienes ${diferencia} años por tanto eres Menor de Edad. No puedes continuar`;
             return
         }
     }
+}
+
+function limpiar() {
+    outEdad.classList.add('inactive');
 }
