5a2fb62ca6ee3fa8d47348bdb3051e5a8efed746
PipeVergara <pipevergara28@hotmail.com>
2023-10-29 12:52:58 -0500

    Mejorando funcionalidad botones de añadir y reducr

--- js/carritoCompras.js
+++ js/carritoCompras.js
@@ -92,11 +92,13 @@ function renderProducts(whiskeys) {
 }
 renderProducts(whiskeys);
 
+function actualizarTotal(valor) {
+    totalDisplay.innerText = valor + ' COP';
+}
+
 function addToContainerCart(e) {
     const codigo = parseInt(e.target.id);
-    /* renderizarEnCarrito(arr); */
     const isThisInCart = verificarSiExiste(codigo);
-    
     function verificarSiExiste(id) {
         const resultadoBuscarEnCarrito = carritoCompras.find((el) => el.codigo === id);
         if (resultadoBuscarEnCarrito) {
@@ -105,22 +107,19 @@ function addToContainerCart(e) {
             return false
         }
     }
-
     function actualizarTotal(valor) {
         totalDisplay.innerText = valor + ' COP';
     }
-
-    function actualizarDOM(quantity,total,codigo) {
+    function actualizarCant(quantity, total, codigo) {
         const quantityOut = document.getElementById(`quantity_product_${codigo}`);
         const totalOut = document.getElementById(`total_product_${codigo}`);
         quantityOut.innerText = quantity;
         totalOut.innerText = total + ' COP';
     }
 
-
     if (!isThisInCart) {
         const whiskey = whiskeys.find((el) => el.codigo === codigo)
-        const producto = {
+        const itemAñadido = {
             codigo: whiskey.codigo,
             name: whiskey.name,
             secondName: whiskey.secondName,
@@ -131,32 +130,31 @@ function addToContainerCart(e) {
             count: 1,
             total: whiskey.price,
         };
-        carritoCompras.push(producto);
-        renderizarEnCarrito(producto);
+        carritoCompras.push(itemAñadido);
+        renderizarEnCarrito(itemAñadido);
         const totalFactura = carritoCompras.reduce((acumulador, el) => acumulador + el.total, 0);
         actualizarTotal(totalFactura);
     } else {
         const whiskey = whiskeys.find((el) => el.codigo === codigo)
         const position = getIndex(whiskey);
-        function getIndex(producto) {
+        function getIndex(itemAñadido) {
             for (let index = 0; index < carritoCompras.length; index++) {
                 const codigo = carritoCompras[index].codigo;
                 if ((codigo === whiskey.codigo)) {
                     return index;
-                }else{'no conseguimmos nada'}
+                } else { 'no conseguimmos nada' }
             }
         }
-        carritoCompras[position].total += carritoCompras[position].price ; 
-        carritoCompras[position].count++; 
+        carritoCompras[position].total += carritoCompras[position].price;
+        carritoCompras[position].count++;
         const cantidad = carritoCompras[position].count;
         const total = carritoCompras[position].total;
         const totalFactura = carritoCompras.reduce((acumulador, el) => acumulador + el.total, 0);
         actualizarTotal(totalFactura);
-        actualizarDOM(cantidad,total,codigo);
-        
+        actualizarCant(cantidad, total, codigo);
+
         return
     }
-
 }
 
 function renderizarEnCarrito(el) {
@@ -180,112 +178,143 @@ function renderizarEnCarrito(el) {
      </div>
  </div> */
 
-
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
-
     /* Creando div class= 'product-card-image' */
     const divProductCartImage = document.createElement('div');
     divProductCartImage.classList.add('product-card-image');
-
     /* Creando img src ='' class ='product-image' */
     const imgCarrito = document.createElement('img');
     imgCarrito.classList.add('product-image');
     imgCarrito.setAttribute('src', el.image);
-
     /* Agregando la imagen al div class = 'product-card-image' */
     divProductCartImage.appendChild(imgCarrito);
-
     /* --------------------------------------- */
-
     /* Creando div class = 'product-card-information-container */
     const divProductCardInformationContainer = document.createElement('div');
     divProductCardInformationContainer.classList.add('product-card-information-container');
-
     /* Crenaod el primer div interno de div class ='product-card-information-container */
     const divPrimerInterno = document.createElement('div');
     divPrimerInterno.classList.add('product-card-information');
-
     /* Creando elementos internos del primerInterno = ' product-card-information' */
     const productName = document.createElement('h3');
     productName.classList.add('product-name');
     productName.innerText = el.name;
     divPrimerInterno.appendChild(productName);
-
     const productSecondName = document.createElement('h3');
     productSecondName.classList.add('product-second-name');
     productSecondName.innerText = el.secondName;
     divPrimerInterno.appendChild(productSecondName);
-
     const productCategory = document.createElement('h4');
     productCategory.classList.add('product-category');
     productCategory.innerText = el.category;
     divPrimerInterno.appendChild(productCategory);
-
     const productVolume = document.createElement('p');
     productVolume.classList.add('product-volume');
     productVolume.innerText = el.volume;
     divPrimerInterno.appendChild(productVolume);
-
     const productPrice = document.createElement('p');
     productPrice.classList.add('product-price');
-    productPrice.setAttribute('id',`total_product_${el.codigo}`);
+    productPrice.setAttribute('id', `total_product_${el.codigo}`);
     productPrice.innerText = el.price + ' COP';
     divPrimerInterno.appendChild(productPrice);
-
     /* Creando segundo interno de div class = 'product-card-information-container' */
-
     const divSegundoInterno = document.createElement('div');
     divSegundoInterno.classList.add('product-card-modificar');
-
     /* Creando elementos interno del segundo Div interno */
     const restarSign = document.createElement('p');
     restarSign.classList.add('restar');
+    restarSign.setAttribute('id', `res_btn_${el.codigo}`);
     restarSign.innerText = '-';
     restarSign.addEventListener('click', reducirCantidad);
     divSegundoInterno.appendChild(restarSign);
-
     const cantidad = document.createElement('p');
     cantidad.classList.add('cantidad');
-    cantidad.setAttribute('id',`quantity_product_${el.codigo}`);
+    cantidad.setAttribute('id', `quantity_product_${el.codigo}`);
     cantidad.innerText = el.count;
     divSegundoInterno.appendChild(cantidad);
-
     const sumarSign = document.createElement('p');
     sumarSign.classList.add('sumar');
+    sumarSign.setAttribute('id', `sum_btn_${el.codigo}`);
     sumarSign.innerText = '+';
     sumarSign.addEventListener('click', agregarCantidad);
     divSegundoInterno.appendChild(sumarSign);
-
     divProductCardInformationContainer.appendChild(divPrimerInterno);
     divProductCardInformationContainer.appendChild(divSegundoInterno);
-
     /* Agregando  Div al Div productCard */
-
     productCard.appendChild(divProductCartImage);
     productCard.appendChild(divProductCardInformationContainer);
-
     /* Agregando productCart a la Contenedor de productos del carrito */
     productCardContainerCart.appendChild(productCard);
-
 }
 
 
+function agregarCantidad(e) {
+    function actualizarCant(quantity, total, codigo) {
+        const quantityOut = document.getElementById(`quantity_product_${codigo}`);
+        const totalOut = document.getElementById(`total_product_${codigo}`);
+        quantityOut.innerText = quantity;
+        totalOut.innerText = total + ' COP';
+    }
+    e.preventDefault();
+    const codigo = Number((e.target.id).slice(8));
+    const item = carritoCompras.find((el) => el.codigo === codigo);
+    const position = getIndex(item);
+    function getIndex(itemAñadido) {
+        for (let index = 0; index < carritoCompras.length; index++) {
+            const codigo = carritoCompras[index].codigo;
+            if ((codigo === item.codigo)) {
+                return index;
+            } else { 'no conseguimmos nada' }
+        }
+    }
 
+    carritoCompras[position].total += carritoCompras[position].price;
+    carritoCompras[position].count++;
 
-function agregarCantidad(obj) {
-    obj.count++;
-    console.log('Agregaron otro del mismo');
-    console.log(obj);
+    const newQuantity = carritoCompras[position].count;
+    const newTotal = carritoCompras[position].total;
+    const newTotalFactura = carritoCompras.reduce((acumulador, el) => acumulador + el.total, 0);
+
+    actualizarCant(newQuantity, newTotal, codigo);
+    actualizarTotal(newTotalFactura);
 
 }
 
 function reducirCantidad(e) {
+    function actualizarCant(quantity, total, codigo) {
+        const quantityOut = document.getElementById(`quantity_product_${codigo}`);
+        const totalOut = document.getElementById(`total_product_${codigo}`);
+        quantityOut.innerText = quantity;
+        totalOut.innerText = total + ' COP';
+    }
+
     e.preventDefault();
-    console.log('eliminaron uno del mismo');
-    console.log(carritoCompras);
+    const codigo = Number((e.target.id).slice(8));
+    const item = carritoCompras.find((el) => el.codigo === codigo);
+    const position = getIndex(item);
+    function getIndex(itemAñadido) {
+        for (let index = 0; index < carritoCompras.length; index++) {
+            const codigo = carritoCompras[index].codigo;
+            if ((codigo === item.codigo)) {
+                return index;
+            } else { 'no conseguimmos nada' }
+        }
+    }
+
+    if (carritoCompras[position].count > 1) {
+        carritoCompras[position].total -= carritoCompras[position].price;
+        carritoCompras[position].count--;
+    } else{ console.log('no puedo');}
 
 
+
+    const newQuantity = carritoCompras[position].count;
+    const newTotal = carritoCompras[position].total;
+    const newTotalFactura = carritoCompras.reduce((acumulador, el) => acumulador + el.total, 0);
+
+    actualizarCant(newQuantity, newTotal, codigo);
+    actualizarTotal(newTotalFactura);
 }
 
 
